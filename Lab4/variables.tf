fd465459b796267671bf8fb298a2d7411fde3f84
Nazar Mykhailiv <73522074+NazarMykhailiv@users.noreply.github.com>
2021-12-24 14:10:50 +0200

    Create variables.tf

---
+++ Lab4/variables.tf
@@ -0,0 +1,11 @@
+variable instance_type {
+  description = "Enter instance_type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable allow_ports{
+    description    = "List with ports"
+    type           = list
+    default        = ["80","443"]
+}
